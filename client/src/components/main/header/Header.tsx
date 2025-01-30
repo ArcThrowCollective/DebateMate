98c715b1ba2f5228e509aef3d1fcf7b549cdd92a
Benjamin L J McCarthy <lukasmacuk@gmail.com>
2025-01-30 11:35:44 +0000

    Added: Components -> Card and Header

--- client/src/components/main/header/Header.tsx
+++ client/src/components/main/header/Header.tsx
@@ -1,14 +1,14 @@
-import Search from '../../UI/inputs/Search';
-
+import './Header.css';
+import { FaUserCircle } from 'react-icons/fa';
 export default function Header() {
   return (
     <>
-      <div id="header" className="flex">
-        <img src="" alt="logo" />
-        <Search />
-        <div className="flex gap-2">
+      <div id="header">
+        <img src={''} alt="logo" />
+        <div className="flex gap-4">
           <button>Login</button>
           <button>Sign Up</button>
+          <FaUserCircle className="avatar" size="2rem" />
         </div>
       </div>
     </>
