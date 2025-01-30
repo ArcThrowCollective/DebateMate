3ddd182be2dae328252bd4eaed02f7bffc60ff22
phno <philipp.norton@posteo.de>
2025-01-30 15:37:05 +0100

    feat: signal offer/answer/candidate

--- signal/src/client_test.ts
+++ signal/src/client_test.ts
@@ -1,17 +1,22 @@
 // run this in node to test socket communication
 
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env' });
+// import dotenv from 'dotenv';
+// dotenv.config({ path: '.env' });
 const { readFileSync } = require('fs');
 const { io } = await import('socket.io-client');
 
+// rejectUnauthorized: false,
 const serverURI = `https://${process.env.SIGNAL_HOST}:${process.env.SIGNAL_PORT}`;
-console.log(`..connecting to signaling server ${serverURI}`)
 const socket = io(serverURI, {
   ca: readFileSync('./cert/signalcert.pem'),
-  rejectUnauthorized: false,
-  query: { user: 'Nody' },
+  auth: {
+    user: 'Nody',
+    id: 666,
+  },
 });
+socket.emit('joinRoom', 'lobby');
+
+console.log(`..connecting to signaling server ${serverURI}`);
 
 socket.on('connect_error', (err) => {
   // the reason of the error, for example "xhr poll error"
@@ -24,3 +29,4 @@ socket.on('connect_error', (err) => {
 
 socket.on('pong', () => console.log('pong'));
 socket.emit('ping');
+socket.emit('joinRoom', 'lobby');
--- signal/src/server.ts
+++ signal/src/server.ts
@@ -7,6 +7,8 @@ import { createServer } from 'node:https';
 import { Server, Socket } from 'socket.io';
 
 dotenv.config({ path: '.env' });
+
+// create https express/socket.io server
 const app = express();
 const httpsServer = createServer(
   {
@@ -15,7 +17,6 @@ const httpsServer = createServer(
   },
   app
 );
-
 const server = createServer(httpsServer);
 const io = new Server(server, {
   cors: {
@@ -29,6 +30,21 @@ declare module 'socket.io' {
     user: string;
     userID: number;
   }
+}
+
+interface signalObj {
+  from: string;
+  to: string;
+  room: string;
+}
+interface offerObj extends signalObj {
+  offer: RTCSessionDescription;
+}
+interface answerObj extends signalObj {
+  answer: RTCSessionDescription;
+}
+interface candidateObj extends signalObj {
+  candidate: RTCIceCandidate;
 }
 
 // inject user ID
@@ -51,18 +67,49 @@ io.use((socket, next) => {
 // });
 
 io.on('connection', (socket) => {
-  console.log(`${socket.user} connected. Rooms: `, socket.rooms);
+  console.log(`# ${socket.user} connected. Rooms: `, socket.rooms);
   socket.on('disconnect', () => {
-    console.log(`${socket.user} disconnected. Rooms: `, socket.rooms);
+    console.log(`# ${socket.user} disconnected. Rooms: `, socket.rooms);
   });
+
+  // SDP offers / answers
+  socket.on('offer', (offerObj: offerObj) => {
+    if (offerObj.offer.type !== 'offer') console.log('offer.type!=offer');
+    socket.broadcast.emit('offer', offerObj);
+    console.log(
+      `o offer from ${offerObj.from} has been broadcast [${offerObj.room}]`
+    );
+  });
+  socket.on('answer', (answerObj: answerObj) => {
+    if (answerObj.answer.type !== 'answer') console.log('answer.type!=answer');
+    socket.broadcast.emit('answer', answerObj);
+    console.log(
+      `o answer from ${answerObj.from} has been broadcast [${answerObj.room}]`
+    );
+  });
+  socket.on('ice-candidate', (candidateObj: candidateObj) => {
+    console.log(
+      `i received iceCandidate from ${candidateObj.from}: ${candidateObj.candidate.candidate} (${candidateObj.candidate.type}) [${candidateObj.room}]`
+    );
+    socket.broadcast.emit('ice-candidate', candidateObj);
+    console.log('... candidate has been broadcast');
+  });
+
+  // rooms
+  socket.on('joinRoom', (room) => {
+    socket.join(room);
+    console.log(`□ ${socket.user} joined room ${room}. Rooms: `, socket.rooms);
+  });
+  socket.on('leaveRoom', (room) => {
+    socket.leave(room);
+    console.log(`□ ${socket.user} joined room ${room}. Rooms: `, socket.rooms);
+  });
+
+  // error handling
   socket.on('ping', (text) => {
     console.log('ping', text);
     io.emit('pong');
   });
-  socket.on('joinRoom', (room) => {
-    socket.join(room);
-    console.log(`${socket.user} joined room ${room}. Rooms: `, socket.rooms)
-  })
   io.engine.on('connection_error', (err) => {
     console.log(err.req); // the request object
     console.log(err.code); // the error code, for example 1
