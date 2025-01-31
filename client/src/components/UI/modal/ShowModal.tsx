7067aff5b5474d2986540046417971d618bd3c7b
Benjamin L J McCarthy <lukasmacuk@gmail.com>
2025-01-31 10:28:24 +0000

    Feature: Added Modals that render forms.

---
+++ client/src/components/UI/modal/ShowModal.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ShowModal.css';
+import { Form } from '../../views/forms/Form';
+import { IoCloseCircle } from 'react-icons/io5';
+
+interface ModalProps {
+  type: 'signup' | 'login' | 'channel' | 'topic' | null;
+  onClose: () => void;
+}
+
+export const Modal: React.FC<ModalProps> = ({ type, onClose }) => {
+  if (!type) return null;
+
+  return (
+    <div className="overlay" onClick={onClose}>
+      <div className="modal__content" onClick={(e) => e.stopPropagation()}>
+        <IoCloseCircle className="modal__close" size="2rem" onClick={onClose} />
+        <Form type={type} onClose={onClose} />
+      </div>
+    </div>
+  );
+};
