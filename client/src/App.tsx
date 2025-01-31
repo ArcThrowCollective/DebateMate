4f83a5908bd45463c97f2a268bd0020d4c29cdb7
Benjamin L J McCarthy <lukasmacuk@gmail.com>
2025-01-31 08:43:13 +0000

    Update: Rooms and Api Service endpoint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,59 @@
+import { Form } from './components/views/forms/Form.tsx';
 import './App.css';
 import Header from './components/main/header/Header.tsx';
-// import Card from './components/UI/cards/Card.tsx';
+// import { RoomList } from './components/UI/cards/RoomList.tsx';
 import { Dashboard } from './components/main/dashboard/Dashboard.tsx';
 
-import { Form } from './components/views/forms/Form.tsx';
+// const roomData = [
+//   {
+//     topic: 'The Universe is Infinite',
+//     imageUrl:
+//       'https://www.nwpc.com/wp-content/uploads/2022/05/placeholder-image-gray-3x2-1.png',
+//     channelInfo: {
+//       name: 'AstroTalks',
+//       avatarUrl: '',
+//     },
+//   },
+//   {
+//     topic: 'The Universe is Infinite',
+//     imageUrl:
+//       'https://www.nwpc.com/wp-content/uploads/2022/05/placeholder-image-gray-3x2-1.png',
+//     channelInfo: {
+//       name: 'AstroTalks',
+//       avatarUrl: '',
+//     },
+//   },
+//   {
+//     topic: 'The Future of AI',
+//     imageUrl:
+//       'https://www.nwpc.com/wp-content/uploads/2022/05/placeholder-image-gray-3x2-1.png',
+//     channelInfo: {
+//       name: 'TechNow',
+//       avatarUrl: '',
+//     },
+//   },
+//   {
+//     topic: 'Debunking Flat Earth',
+//     imageUrl:
+//       'https://www.nwpc.com/wp-content/uploads/2022/05/placeholder-image-gray-3x2-1.png',
+//     channelInfo: {
+//       name: 'ScienceDaily',
+//       avatarUrl: '',
+//     },
+//   },
+// ];
 
 function App() {
   return (
-    <>
-      <Dashboard>
-        <Header />
-        {/* <Card /> */}
-        <Form type="signup" />
-        <Header />
-      </Dashboard>
-    </>
+    <Dashboard>
+      <Header />
+      <div className="flex flex-col self-center w-[90vw] gap-10 text-center">
+        {/* <h1 className="text-3xl font-bold">Active Topics</h1> */}
+        <Form type="channel" />
+        {/* <RoomList rooms={roomData} /> */}
+      </div>
+      <Header />
+    </Dashboard>
   );
 }
 
---
+++ client/src/components/UI/cards/RoomList.tsx
@@ -0,0 +1,26 @@
+import Room from './Room';
+
+interface ChannelInfo {
+  name: string;
+  avatarUrl: string;
+}
+
+export interface RoomData {
+  topic: string;
+  imageUrl: string;
+  channelInfo: ChannelInfo;
+}
+
+interface RoomListProps {
+  rooms: RoomData[];
+}
+
+export const RoomList: React.FC<RoomListProps> = ({ rooms }) => {
+  return (
+    <div className="flex flex-wrap justify-center gap-6 p-4">
+      {rooms.map((roomData, index) => (
+        <Room key={index} roomID={index} roomData={roomData} />
+      ))}
+    </div>
+  );
+};
