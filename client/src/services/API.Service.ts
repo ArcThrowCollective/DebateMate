6636f98de9dbb52d2d95bb5bee674677e28123cd
Benjamin L J McCarthy <lukasmacuk@gmail.com>
2025-02-03 10:12:45 +0000

    Fix: API route

--- client/src/services/API.Service.ts
+++ client/src/services/API.Service.ts
@@ -9,7 +9,7 @@ export const submitFormData = async (
   formData: Record<string, any>
 ) => {
   try {
-    const response = await axios.post(`${URL}/api`, formData, {
+    const response = await axios.post(`${URL}/${formType}`, formData, {
       headers: {
         'Content-Type': 'application/json',
       },
