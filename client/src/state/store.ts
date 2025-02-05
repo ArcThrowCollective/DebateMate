eaf1669b1fa449e57dbd64b6d545b4a3db2359b6
Benjamin L J McCarthy <lukasmacuk@gmail.com>
2025-02-05 12:15:28 +0000

    Feature: Voting + Button Component update.

--- client/src/state/store.ts
+++ client/src/state/store.ts
@@ -1,12 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import navigationReducer from './navigation/navigationSlice';
 import loaderReducer from './loader/loaderSlice';
+import streamReducer from './stream/streamSlice';
+import voteReducer from './vote/voteSlice';
 
 export const store = configureStore({
   reducer: {
     navigation: navigationReducer,
     loader: loaderReducer,
+    stream: streamReducer,
+    votes: voteReducer,
   },
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
 export type RootState = ReturnType<typeof store.getState>;
