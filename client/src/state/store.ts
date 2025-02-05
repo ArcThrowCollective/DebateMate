038f083049ef2c225e75b0a312ae03baf1df5f67
toldpixel <software.nutshell@gmail.com>
2025-02-05 18:12:44 +0100

    Added: HeadViewScreen room Title

--- client/src/state/store.ts
+++ client/src/state/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import navigationReducer from './navigation/navigationSlice';
 import loaderReducer from './loader/loaderSlice';
 import voteReducer from './vote/voteSlice';
+import timerReducer from './timer/timerSlice';
 
 export const store = configureStore({
   reducer: {
     navigation: navigationReducer,
     loader: loaderReducer,
     votes: voteReducer,
+    timer: timerReducer,
   },
   devTools: process.env.NODE_ENV !== 'production',
 });
